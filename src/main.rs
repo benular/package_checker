f36ad383b15d31a92bb7f10a5774eaf9b2cf802b
finn <finn@finnloske.de>
2025-09-16 23:48:32 +0200

    added get_aur_helper+ linting

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,94 @@
-use std::{fs::{File, OpenOptions}, io::Write, process::Command};
-fn main(){
-    let mut log= File::create("log.md");
+use chrono::Local;
+use std::{
+    fs::{File, OpenOptions},
+    io::Write,
+    process::Command,
+};
+fn main() {
+    let mut audit = match OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(generate_logname())
+    {
+        Ok(file) => file,
+        Err(error) => {
+            eprint!("failed to create new audit {}", error);
+            return; //smt like break available?
+        }
+    };
+    let aur_helper:&str=get_aur_helper();
 
-    let installed_pkgs=Command::new("pacman").arg("-Q").output();
-
- 
+    // try to run the python file? else reimplement
+    // let installed_pkgs=Command::new("pacman").arg("-Q").output();
+    // Setup User Best Practices
+    setup_user(&mut audit);
 }
 
-pub fn run_command(line:&str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let output =Command::new("cat").arg(&path).output()?;
+pub fn get_aur_helper()-> &'static str {
+    match Command::new("yay").arg("-V").output() {
+        Ok(output) if output.status.success()=>"yay",
+        _ => match Command::new("paru").arg("-V").output() {
+            Ok(output) if output.status.success()=>"paru",   
+           _ => "none"
+        }   
+    }
+} 
 
-    let file_content =String::from_utf8(output.stdout)?;
-    if !file_content.contains(&line) {
+pub fn run_command(line: &str, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let file = std::fs::read_to_string(path)?;
+    if !file.contains(line) {
         let mut echo_command = Command::new("sudo");
-        echo_command.arg("sh").arg("-c").arg(format!("echo '{}' >> {}", line,path));
+        echo_command
+            .arg("sh")
+            .arg("-c")
+            .arg(format!("echo '{}' >> {}", line, path));
         echo_command.status()?;
     }
     Ok(())
 }
 
-pub fn  setup_user(){
+pub fn setup_user(log: &mut File) {
     // implement user setup best practices from the user tab
     // setting delay after failed login attempts
-    let log= OpenOptions::new().create(true).append(true).open(generate__logname());
-    if !run_command("auth optional pam_faildelay.so delay=3000000", "/etc/pam.d/system-login").is_err(){
-        log.write_fmt("added delay after failed login");
+    if run_command(
+        "auth optional pam_faildelay.so delay=3000000",
+        "/etc/pam.d/system-login",
+    )
+    .is_ok()
+    {
+        let _ = writeln!(log, "added delay after failed login");
     }
-    
+
     //disable ssh logins for root
-    if !run_command("PermitRootLogin no","/etc/ssh/sshd_config.d/20-deny-root.conf" ).is_err(){
-        log.write_fmt("removed root ssh access");
+    if run_command(
+        "PermitRootLogin no",
+        "/etc/ssh/sshd_config.d/20-deny-root.conf",
+    )
+    .is_ok()
+    {
+        let _ = writeln!(log, "removed root ssh access");
     };
 
-    //limit processes
-    if !run_command("soft nproc 7000", "/etc/security/limits.conf").is_err(){
-
+    //limit processes to 7000
+    if run_command("soft nproc 7000", "/etc/security/limits.conf").is_ok() {
+        let _ = writeln!(log, "added process limit");
     }
-
 }
-fn generate__logname()-> &str{
+fn generate_logname() -> String {
     // generates the name of the logfile -> returns for example "audit2025-123-31"
     let date = Local::now().format("%Y-%m-%d").to_string();
-    return ("audit_{}",date)
-
+    format!("audit_{}.md", date)
 }
+
+//TODO: before implementation test hardend_malloc to check for issues and chrashes(seems to crash
+//frequently with firefox, docker steam and wine)
+// TODO: Function that lets the user either decide per package case if hardend_malloc should be
+// used or not(depending on installed_pkgs)
+pub fn harden_memory(log:&mut File) {
+    let aur_helper_installed:bool=
+}
+//TODO reencrypt the boot drive -> needs test env!
+pub fn encrypt_drive(audit: &mut File) {}
+
+// TODO install Linux_kernel_hardened, set it to default in bootloader conf (grub or systemd?)
+pub fn harden_kernel() {}
